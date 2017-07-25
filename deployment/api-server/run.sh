c2396bdc6583ec6d25b048ee88287e15379071fd
mikechiu <mikechiu@emotibot.com>
2017-07-25 16:39:32 +0800

    Modify run.sh to support multiple services

--- deployment/api-server/run.sh
+++ deployment/api-server/run.sh
@@ -7,14 +7,20 @@ if [ "$?" -ne 0 ]; then
   echo "Erorr, can't open envfile: $1"
   echo "Usage: $0 <env file>"
   echo "e.g., "
-  echo " $0 api-sh.env [SERVICE]"
+  echo " $0 api-sh.env [SERVICE1] [SERVICE2]..."
   exit 1
 else
   envfile=$1
   echo "# Using envfile: $envfile"
 fi
+shift
 
-service=$2
+while [ $# != 0 ]
+do
+    echo $1
+    service="$service "$1
+    shift
+done
 
 # prepare docker-compose env file
 cp $envfile .env
