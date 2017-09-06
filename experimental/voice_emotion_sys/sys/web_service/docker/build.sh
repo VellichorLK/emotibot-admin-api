774f2525e14b2fc2bd0a2869e792af6f562b1d74
DanielWu <danielwu@emotibot.com>
2017-09-06 12:07:03 +0800

    [ecovacs] Update api tag

--- experimental/voice_emotion_sys/sys/web_service/docker/build.sh
+++ experimental/voice_emotion_sys/sys/web_service/docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=voice_emotion_reg
-TAG=2017082101
+TAG=2017090601
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
--- experimental/voice_emotion_sys/sys/web_service/docker/run.sh
+++ experimental/voice_emotion_sys/sys/web_service/docker/run.sh
@@ -10,7 +10,7 @@ CONTAINER=voice_emotion_reg
 # </EDIT_ME>
 
 # Get tags from args
-TAG=2017082101
+TAG=2017090601
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 echo "# Launching $DOCKER_IMAGE"
 # Check if docker image exists (locally or on the registry)
