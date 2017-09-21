2b1734ed648fcba4fac7ca4684d07606c2019864
mikechiu <mikechiu@emotibot.com>
2017-09-21 16:43:29 +0800

    [VECS-1.0]: fix problem report duration divide by 1000 twice.
    root cause: should update report.go rather than render.go.
    recovery render.go and update report.go

--- experimental/voice_emotion_sys/sys/vendor/handlers/report.go
+++ experimental/voice_emotion_sys/sys/vendor/handlers/report.go
@@ -112,7 +112,8 @@ func (r *ReportCtx) openReport() error {
 func finishJSONReport(r *Report, writeToFile bool, f *os.File) ([]byte, string, error) {
 
 	for _, v := range r.Records {
-		duration := float64(v.Duration) / 1000
+		//duration := float64(v.Duration) / 1000
+		duration := float64(v.Duration)
 		r.CumulativeDuration += duration
 		r.Count++
 
--- experimental/voice_emotion_sys/sys/web_service/docker/build.sh
+++ experimental/voice_emotion_sys/sys/web_service/docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=voice_emotion_reg
-TAG=2017092001
+TAG=2017092101
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
