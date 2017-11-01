e175bac668da9bd7234620aa82606e0eb34e807e
DanielWu <i.lu.f@hotmail.com.tw>
2017-11-01 10:38:15 +0800

    Update vipshop api ver to 2017110101

--- module/vipshop-admin/docker/build.sh
+++ module/vipshop-admin/docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=vipshop-admin-api
 # TAG="$(git rev-parse --short HEAD)"
-TAG="2017103001"
+TAG="2017110101"
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
