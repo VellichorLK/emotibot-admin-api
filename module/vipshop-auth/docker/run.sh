d224b55dda00282786d764b214d8325c883f41f5
DanielWu <i.lu.f@hotmail.com.tw>
2017-11-16 15:58:14 +0800

    Update fake-cauth to 2017111603 and adapter to 2017111602

--- module/vipshop-auth/docker/run.sh
+++ module/vipshop-auth/docker/run.sh
@@ -4,7 +4,7 @@ REPO=docker-reg.emotibot.com.cn:55688
 # The name of the container, should use the name of the repo is possible
 CONTAINER=vipshop-auth-adapter
 #TAG="$(git rev-parse --short HEAD)"
-TAG="2017111601"
+TAG="2017111602"
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 globalConf="
