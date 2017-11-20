e64fcdc6e960fc0befa48381f1121a1a95883961
taylorchung <taylorchung@emotibot.com>
2017-11-20 18:38:37 +0800

    CAS parameters to env file

--- module/vipshop-auth/docker/build.sh
+++ module/vipshop-auth/docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=vipshop-auth-adapter
 # TAG="$(git rev-parse --short HEAD)"
-TAG="2017111608-fortest"
+TAG="2017112001"
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
