d46df0bcc988c37bfa64581312f1a4a2b761b43f
mikechiu <mikechiu@emotibot.com>
2017-11-23 14:22:49 +0800

    update vipshop-auth env to fullfile bot-config image building

--- module/vipshop-auth/docker/build.sh
+++ module/vipshop-auth/docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=vipshop-auth-adapter
 # TAG="$(git rev-parse --short HEAD)"
-TAG="2017112003"
+TAG="2017113001"
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
