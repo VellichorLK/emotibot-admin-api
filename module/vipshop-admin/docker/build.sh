69235fb2218460bd206c2e050fbed4d223f4b15d
DanielWu <danielwu@emotibot.com>
2017-11-24 14:45:56 +0800

    Update api to 20171124-fe27a1f

--- module/vipshop-admin/docker/build.sh
+++ module/vipshop-admin/docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=vipshop-admin-api
 # TAG="$(git rev-parse --short HEAD)"
-LAST_RELEASE_TAG="20171123-d46df0b"
+LAST_RELEASE_TAG="20171124-fe27a1f"
 GIT_HEAD="$(git rev-parse --short HEAD)"
 DATE=`date +%Y%m%d`
 TAG=$1
--- module/vipshop-admin/docker/run.sh
+++ module/vipshop-admin/docker/run.sh
@@ -4,7 +4,7 @@ REPO=docker-reg.emotibot.com.cn:55688
 # The name of the container, should use the name of the repo is possible
 CONTAINER=vipshop-admin-api
 #TAG="$(git rev-parse --short HEAD)"
-LAST_RELEASE_TAG="20171123-d46df0b"
+LAST_RELEASE_TAG="20171124-fe27a1f"
 
 TAG=$2
 if [ "$TAG" == "" ]; then
