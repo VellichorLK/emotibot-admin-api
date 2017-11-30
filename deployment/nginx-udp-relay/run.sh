ca355e652e52135bd9a0fef8c07585ba92f966c5
mikechiu <mikechiu@emotibot.com>
2017-11-30 09:18:24 +0800

    Add nginx-udp-relay to relay udp packets from datadot agent to central server (10.0.0.45)

---
+++ deployment/nginx-udp-relay/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+DOCKER_IMAGE=nginx:1-alpine
+DOCKER_CONTAINER=nginx-udp-relay
+
+cmd="docker pull ${DOCKER_IMAGE}"
+echo $cmd
+eval $cmd
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cmd="docker rm -f ${DOCKER_CONTAINER}; docker run -d --name=${DOCKER_CONTAINER} -p 8125:8125/udp -v $DIR/nginx.conf:/etc/nginx/nginx.conf ${DOCKER_IMAGE}"
+echo $cmd
+eval $cmd
