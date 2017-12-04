6e8946ffe08266f47116cf25bce7e6d61a4cb7b4
Should_Not_Push_From_Me <deployer@emotibot.com>
2017-12-04 19:42:08 +0800

    remove unused vip env

--- deployment/proxy-config/idc1/haproxy-idc1/run.sh
+++ deployment/proxy-config/idc1/haproxy-idc1/run.sh
@@ -2,15 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [ "$#" -lt 1 ] ; then
-  echo "should give environment parameter"
-  echo "ex: ./run.sh default"
-  echo "ex: or ./run.sh vip"
-  exit 1
-fi
-
-ENV=$1
-
 NAME='haproxy-idc1'
 
 docker rm -fv $NAME
@@ -19,7 +10,7 @@ cmd="docker run -d --name $NAME\
   -p 9001:9001 \
   -p 9010:9010 \
   -p 9527:9527 \
-  -v $DIR/haproxy.${ENV}.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
+  -v $DIR/haproxy.default.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro \
   haproxy:1.6"
  
 echo $cmd
