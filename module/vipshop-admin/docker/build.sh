5e6674e7157b2f611c89eba813e99d5ddeb5b47b
DanielWu <i.lu.f@hotmail.com.tw>
2017-12-12 10:53:36 +0800

    Update vipshop-auth to 20171212001 and admin to 20171212-2c95de7
    Fixes VIP-428, VIP-429, VIP-455, VIP-549

--- module/vipshop-admin/docker/build.sh
+++ module/vipshop-admin/docker/build.sh
@@ -2,7 +2,7 @@
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=vipshop-admin-api
 # TAG="$(git rev-parse --short HEAD)"
-LAST_RELEASE_TAG="20171208-cea50e8"
+LAST_RELEASE_TAG="20171212-2c95de7"
 GIT_HEAD="$(git rev-parse --short HEAD)"
 DATE=`date +%Y%m%d`
 TAG=$1
--- module/vipshop-auth/docker/run.sh
+++ module/vipshop-auth/docker/run.sh
@@ -4,7 +4,7 @@ REPO=docker-reg.emotibot.com.cn:55688
 # The name of the container, should use the name of the repo is possible
 CONTAINER=vipshop-auth-adapter
 #TAG="$(git rev-parse --short HEAD)"
-TAG="20171204001"
+TAG="20171212001"
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 globalConf="
