34156f506b09f2c53338c8bdb3043707eb382c51
kenchang <kenchang@emotibot.com>
2018-02-26 18:26:53 +0800

    add data structures

--- module/vipshop-admin/FAQ/type.go
+++ module/vipshop-admin/FAQ/type.go
@@ -22,3 +22,56 @@ type Category struct {
 	Name     string
 	ParentID int
 }
+
+type Question struct {
+	QuestionId int `json:questionId`
+	SQuestionConunt int `json:sQuesCount`
+	Content string `json:questionContent`
+	CategoryName string `json:categoryName`
+	CategoryId int `json:categoryId`
+	Answers []Answer `json:answerItem`
+}
+
+type Answer struct {
+	QuestionId int `json:Question_Id`
+	AnswerId int `json:Answer_Id`
+	Content string `json:Content_String`
+	RelatedQuestion []string `json:RelatedQuestion`
+	DynamicMenu []string `json:DynamicMenu`
+	NotShow int `json:Not_Show_In_Relative_Q`
+	BeginTime string `json:Begin_Time`
+	EndTime string `json:End_Time`
+	AnswerCmd string `json:Answer_CMD`
+	AnswerCmdMsg string `json:Answer_CMD_Msg`
+	Dimension []string `json:dimension`
+}
+
+type QueryCondition struct {
+	TimeSet bool
+	BeginTime string
+	EndTime string
+	Keyword string
+	SearchQuestion bool
+	SearchAnswer bool
+	SearchDynamicMenu bool
+	SearchRelativeQuestion bool
+	NotShow bool
+	Dimension []DimensionGroup
+	CategoryId int
+	Limit int
+	CurPage int
+}
+
+type DimensionGroup struct {
+	TypeId int `json:"typeId"`
+	Content string `json:"tagContent"`
+}
+
+type Parameter interface {
+	FormValue(name string) string
+}
+
+type Tag struct {
+	Type int
+	Content string
+}
