b71ae1afc6c09b8c4fdc06e455adf5c4c034b783
Su Ting Wen <zmes50416@gmail.com>
2018-03-29 18:05:16 +0800

    change image name to lower-case.

--- experimental/RFImporter/build.sh
+++ experimental/RFImporter/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-cmd="docker build -t docker-reg.emotibot.com.cn:55688/RFImporter -f $DIR/dockerfile $DIR"
+cmd="docker build -t docker-reg.emotibot.com.cn:55688/rf-importer -f $DIR/dockerfile $DIR"
 echo $cmd
 eval $cmd
 
