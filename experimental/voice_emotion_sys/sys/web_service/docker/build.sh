f5903b3f3c9fb64f818328c56b369f1c85388bf7
taylorchung <taylorchung@emotibot.com>
2018-04-11 10:14:35 +0800

    fix bug, end time align

--- experimental/voice_emotion_sys/sys/web_service/docker/build.sh
+++ experimental/voice_emotion_sys/sys/web_service/docker/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=voice_emotion_reg
-TAG=2018041000
+TAG=2018041100
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
--- experimental/voice_emotion_sys/sys/web_service/docker/run.sh
+++ experimental/voice_emotion_sys/sys/web_service/docker/run.sh
@@ -10,7 +10,7 @@ CONTAINER=voice_emotion_reg
 # </EDIT_ME>
 
 # Get tags from args
-TAG=2018041000
+TAG=2018041100
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 echo "# Launching $DOCKER_IMAGE"
 # Check if docker image exists (locally or on the registry)
