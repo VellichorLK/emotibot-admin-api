4713c1cfec832659339335debbdce08d6a56c50f
Su Ting Wen <zmes50416@gmail.com>
2018-04-13 15:32:21 +0800

    Update systex to 0.0.3 version
    * Add csv logging for APIs.

---
+++ module/systex-controller/controller.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/siongui/gojianfan"
+)
+
+type v2TextResponse struct {
+	Text string `json:"text"`
+}
+
+func voiceToTextHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Only support POST method.")
+		return
+	}
+
+	if r.Header.Get("content-type") != "audio/x-wav" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "content-type only support audio/x-wav.")
+		return
+	}
+	sentence, err := asrClient.Recognize(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, err)
+		return
+	}
+	var resp v2TextResponse
+	log.Printf("v2Text: %s\n", sentence)
+	textLogger.Write([]string{time.Now().String(), sentence})
+	textLogger.Flush()
+	resp.Text = sentence
+	data, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, string(data))
+}
+
+func voiceToTaskHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprintln(w, "Only support POST method.")
+		return
+	}
+
+	if r.Header.Get("content-type") != "audio/x-wav" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "content-type only support audio/x-wav.")
+		return
+	}
+	var userID = r.Header.Get("X-UserID")
+	if userID == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintln(w, "UserID is empty")
+		return
+	}
+
+	sentence, err := asrClient.Recognize(r.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "recognize api error:"+err.Error())
+		return
+	}
+	sentence, err = csClient.Simplify(sentence)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "cuservice api error:"+err.Error())
+		return
+	}
+
+	resp, err := teClient.ET(userID, sentence)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintln(w, "Task Engine api error:"+err.Error())
+		return
+	}
+	respStr := string(resp)
+	respStr = gojianfan.S2T(respStr)
+	log.Printf("v2Task: %s, te result: %s\n", sentence, respStr)
+	taskEngineLogger.Write([]string{time.Now().String(), sentence, respStr})
+	taskEngineLogger.Flush()
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, respStr)
+
+}
--- module/systex-controller/main.go
+++ module/systex-controller/main.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+	"encoding/csv"
 	"log"
 	"net/http"
 	"net/url"
@@ -12,7 +11,6 @@ import (
 	"emotibot.com/emotigo/module/systex-controller/api/asr"
 	"emotibot.com/emotigo/module/systex-controller/api/cuservice"
 	"emotibot.com/emotigo/module/systex-controller/api/taskengine"
-	"github.com/siongui/gojianfan"
 )
 
 //api global client for ASR module
@@ -28,90 +26,23 @@ var requiredEnvs = []string{
 	"APPID",   //APPID for task engine
 }
 
-type v2TextResponse struct {
-	Text string `json:"text"`
-}
-
-func voiceToTextHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Only support POST method.")
-		return
-	}
-
-	if r.Header.Get("content-type") != "audio/x-wav" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, "content-type only support audio/x-wav.")
-		return
-	}
-	sentence, err := asrClient.Recognize(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, err)
-		return
-	}
-	var resp v2TextResponse
-	log.Printf("v2Text: %s\n", sentence)
-	resp.Text = sentence
-	data, err := json.Marshal(resp)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintln(w, string(data))
-}
-
-func voiceToTaskHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Only support POST method.")
-		return
-	}
-
-	if r.Header.Get("content-type") != "audio/x-wav" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, "content-type only support audio/x-wav.")
-		return
-	}
-	var userID = r.Header.Get("X-UserID")
-	if userID == "" {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, "UserID is empty")
-		return
-	}
-
-	sentence, err := asrClient.Recognize(r.Body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, "recognize api error:"+err.Error())
-		return
-	}
-	sentence, err = csClient.Simplify(sentence)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, "cuservice api error:"+err.Error())
-		return
-	}
-
-	resp, err := teClient.ET(userID, sentence)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprintln(w, "Task Engine api error:"+err.Error())
-		return
-	}
-	respStr := string(resp)
-	respStr = gojianfan.S2T(respStr)
-	log.Printf("v2Task: %s, te result: %s\n", sentence, respStr)
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintln(w, respStr)
-
-}
+var textLogger *csv.Writer
+var taskEngineLogger *csv.Writer
 
 func main() {
+	csvFile, err := os.Create("./v2Text.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+	csvFile2, err := os.Create("./v2TE.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+	textLogger = csv.NewWriter(csvFile)
+	taskEngineLogger = csv.NewWriter(csvFile2)
+
 	var env = make(map[string]string)
 	for _, key := range requiredEnvs {
 		value, ok := os.LookupEnv(key)
