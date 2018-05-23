5f5107885675b558ef70bb77ee14750a50c2a27f
Frank Chang <frankchang@emotibot.com>
2018-05-23 13:14:42 +0800

    Add dictionary_v3_20180523.sql

---
+++ module/admin-api/db_migrate/dictionary_v3_20180523.sql
@@ -0,0 +1,77 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 172.16.101.143 (MySQL 5.7.20)
+# Database: emotibot
+# Generation Time: 2018-05-14 04:05:23 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table entities
+# ------------------------------------------------------------
+
+CREATE TABLE `entities` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `editable` tinyint(1) DEFAULT NULL,
+  `cid` int(20) NOT NULL,
+  `similar_words` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `answer` varchar(200) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table entity_class
+# ------------------------------------------------------------
+
+CREATE TABLE `entity_class` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
+  `appid` char(36) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `name` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `pid` int(20) DEFAULT NULL,
+  `editable` tinyint(1) NOT NULL DEFAULT '1',
+  `intent_engine` tinyint(1) NOT NULL DEFAULT '0',
+  `rule_engine` tinyint(1) NOT NULL DEFAULT '1',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+
+# Dump of table entity_files
+# ------------------------------------------------------------
+
+CREATE TABLE `entity_files` (
+  `id` int(18) NOT NULL AUTO_INCREMENT,
+  `appid` char(32) COLLATE utf8_unicode_ci NOT NULL,
+  `filename` varchar(64) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
+  `content` longblob NOT NULL,
+  `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `appid` (`appid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
