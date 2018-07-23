7cc477c803069bc068efec37ce58baab2b31b365
Su Ting Wen <zmes50416@gmail.com>
2018-07-23 14:30:34 +0800

    Clean up docker build & run flow
    - run.sh now dadicated to run as development env only
    - run.sh now read the same env file as go run server.go test.env
    - remove deprecated buildBash script & dockerfile
    - add add_prefix.sh scipt to transform origin env into production env

--- module/admin-api/docker/buildBase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-GIT_HEAD="$(git rev-parse --short HEAD)"
-IMAGE="docker-reg.emotibot.com.cn:55688/admin-api:$GIT_HEAD-benchmarkOnly"
-BUILDROOT=$PWD/../../
-MODULE="admin-api"
-echo "Module: $MODULE"
-docker build -t $IMAGE -f baseDockerfile   --build-arg PROJECT=$MODULE $BUILDROOT
--- module/admin-api/docker/run.sh
+++ module/admin-api/docker/run.sh
@@ -5,8 +5,9 @@ REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=admin-api
 #TAG="$(git rev-parse --short HEAD)"
 LAST_RELEASE_TAG="latest"
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-TAG=$2
+TAG=$1
 if [ "$TAG" == "" ]; then
     TAG="$LAST_RELEASE_TAG"
 fi
@@ -14,20 +15,24 @@ DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 if [ ${USER} == "deployer" ]; then
 globalConf="
-    -v /etc/localtime:/etc/localtime \
+    -e TZ=Asia/Taipei \
     --restart always \
 "
 fi
+
+# entrypoint rewrite & env is dev only
+# DO NOT DO THIS IN PRODUCTION
 moduleConf="
-    -p 8181:8181
-    --env-file $1
+    -p 8182:8182
+    -v $DIR/entrypoint-env.sh:/usr/local/bin/entrypoint.sh:ro
+    -v $DIR/../test.env:/usr/local/bin/.env:ro
 "
 
 docker rm -f $CONTAINER
 cmd="docker run -d --name $CONTAINER \
     $globalConf \
     $moduleConf \
-    $DOCKER_IMAGE \
+    $DOCKER_IMAGE
 "
 
 echo $cmd
