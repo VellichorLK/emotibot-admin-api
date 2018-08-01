ae6e54056f6f327f705265e84509e68c64ae5837
Frank Chang <frankchang@emotibot.com>
2018-08-01 18:46:27 +0800

    admin-ui: Statistic supports indices partition by log date
    * For records indices:
        Concatenates indices name with 'log_time', (ex: records-2018.08.01)
    * For sessions indices:
        Concatenates indices name with 'end_time', (ex: sessions-2018.08.01)

--- module/admin-api/ELKStats/data/constants.go
+++ module/admin-api/ELKStats/data/constants.go
@@ -17,11 +17,13 @@ const (
 	AppIDHeaderKey = "X-AppID"
 )
 
-const ESRecordsMappingFile = "./InitFiles/elasticsearch/configs/records_mapping.json"
+const ESRecordsTemplate = "emotibot_records_template"
+const ESRecordsTemplateFile = "./InitFiles/elasticsearch/configs/records_template.json"
 const ESRecordsIndex = "emotibot-records-*"
 const ESRecordType = "doc"
 
-const ESSessionsMappingFile = "./InitFiles/elasticsearch/configs/sessions_mapping.json"
+const ESSessionsTemplate = "emotibot_sessions_template"
+const ESSessionsTemplateFile = "./InitFiles/elasticsearch/configs/sessions_template.json"
 const ESSessionsIndex = "emotibot-sessions-*"
 const ESSessionsType = "doc"
 
