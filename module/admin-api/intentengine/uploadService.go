e5163ff4551515565e2334a8c99b1026d99d699b
DanielWu <i.lu.f@hotmail.com.tw>
2018-08-02 12:27:36 +0800

    FEATURE: Can import intent with BF2 format

--- module/admin-api/intentengine/uploadService.go
+++ module/admin-api/intentengine/uploadService.go
@@ -2,8 +2,10 @@ package intentengine
 
 import (
 	"errors"
+	"strings"
 
 	"emotibot.com/emotigo/module/admin-api/util"
+	"emotibot.com/emotigo/module/admin-api/util/localemsg"
 	"github.com/siongui/gojianfan"
 	"github.com/tealeg/xlsx"
 )
@@ -23,6 +25,37 @@ func ParseIntentsFromXLSX(file []byte) (ret map[string][]string, err error) {
 		return nil, errors.New(util.Msg["SheetError"])
 	}
 
+	for idx := range sheets {
+		if sheets[idx].Name == localemsg.Get("zh-cn", "IntentBF2Sheet1Name") ||
+			sheets[idx].Name == localemsg.Get("zh-tw", "IntentBF2Sheet1Name") {
+			return parseBF2IntentsFormat(sheets[idx])
+		}
+	}
+	return parseBFOPIntentsFormat(sheets)
+}
+
+func parseBF2IntentsFormat(sheet *xlsx.Sheet) (ret map[string][]string, err error) {
+	ret = make(map[string][]string)
+
+	for _, row := range sheet.Rows {
+		cells := row.Cells
+		if cells != nil && len(cells) > 1 {
+			intent := strings.TrimSpace(cells[0].String())
+			sentence := strings.TrimSpace(cells[1].String())
+			if intent != "" && sentence != "" {
+				if _, ok := ret[gojianfan.T2S(sheet.Name)]; !ok {
+					ret[gojianfan.T2S(sheet.Name)] = []string{}
+				}
+				ret[gojianfan.T2S(sheet.Name)] = append(
+					ret[gojianfan.T2S(sheet.Name)], gojianfan.T2S(sentence))
+			}
+		}
+	}
+
+	return
+}
+
+func parseBFOPIntentsFormat(sheets []*xlsx.Sheet) (ret map[string][]string, err error) {
 	ret = make(map[string][]string)
 
 	for _, sheet := range sheets {
