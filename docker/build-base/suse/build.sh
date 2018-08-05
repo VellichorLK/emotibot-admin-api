5581065054a626f9e3b3f542d3035c9a39569d17
DanielWu <i.lu.f@hotmail.com.tw>
2018-08-06 06:59:53 +0800

    REFACTOR: update build bash script to chooice platform

--- docker/build-base/suse/build.sh
+++ docker/build-base/suse/build.sh
@@ -3,14 +3,21 @@ set -e
 
 REPO=docker-reg.emotibot.com.cn:55688
 CONTAINER=gobase-suse
-TAG=1.9-alpine
+VERSION=$1
+TAG=$GOVER-alpine
 DOCKER_IMAGE=$REPO/$CONTAINER:$TAG
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 printf $DOCKER_IMAGE > $DIR/DOCKER_IMAGE
 
+echo -n "Trying to pull image: $DOCKER_IMAGE...";
+docker pull $DOCKER_IMAGE > /dev/null 2>&1 && echo "OK" && exit 0;
+echo "FAIL";
+echo "Start to build base platform image"
+
 cmd="docker build \
+        --build-arg VERSION=$VERSION \
         -t $DOCKER_IMAGE \
         -f Dockerfile ."
 echo $cmd
-eval $cmd
+eval $cmd && docker push $DOCKER_IMAGE;
