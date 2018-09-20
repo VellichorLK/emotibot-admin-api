e78b917daf30c8958472c4a9dd59fb4d395bbc61
Su Ting Wen <zmes50416@gmail.com>
2018-09-20 21:29:22 +0800

    Fix: Mock admin-api unit-test
    Mock auth unit test to independent of internal server.
    Comment out unfinish test.

--- module/admin-api/auth/dao_test.go
+++ module/admin-api/auth/dao_test.go
@@ -1,47 +1,59 @@
 package auth
 
 import (
-	"fmt"
+	"database/sql"
+	"os"
 	"testing"
 
 	"emotibot.com/emotigo/module/admin-api/util"
+	"gopkg.in/DATA-DOG/go-sqlmock.v1"
 )
 
+var writer sqlmock.Sqlmock
+
+func TestMain(m *testing.M) {
+	var db *sql.DB
+	db, writer, _ = sqlmock.New()
+	util.SetDB(ModuleInfo.ModuleName, db)
+	os.Exit(m.Run())
+}
 func TestGetUsername(t *testing.T) {
-	dao, err := util.InitDB("172.16.101.98", "root", "password", "auth")
-	if err != nil {
-		t.Error("Cannot init auth db")
-		return
-	}
-
-	util.SetDB(ModuleInfo.ModuleName, dao)
-
+	var mockedRows = sqlmock.NewRows([]string{"uuid", "user_name"}).AddRow("0", "Dean").AddRow("1", "Daniel")
+	writer.ExpectQuery("SELECT ").WithArgs("4b21158a395311e88a710242ac110003").WillReturnRows(mockedRows)
 	usernames, err := GetUserNames([]string{"4b21158a395311e88a710242ac110003"})
 	if err != nil {
-		t.Error("GetUsernames fail", err.Error())
+		t.Fatal(err)
 	}
-	fmt.Printf("Get usernames: %+v\n", usernames)
-
+	if len(usernames) != 2 {
+		t.Fatal("user expect to be 2 but got ", len(usernames))
+	}
+	mockedRows = sqlmock.NewRows([]string{"enterprise"}).AddRow("APPLE")
+	writer.ExpectQuery("SELECT enterprise FROM apps").WithArgs("csbot").WillReturnRows(mockedRows)
+	mockedRows = sqlmock.NewRows([]string{"uuid", "user_name"}).AddRow("0", "Dean").AddRow("1", "Daniel")
+	writer.ExpectQuery("SELECT uuid, user_name FROM users").WithArgs("APPLE").WillReturnRows(mockedRows)
 	allnames, err := GetAllUserNames("csbot")
 	if err != nil {
-		t.Error("GetUsernames fail", err.Error())
+		t.Fatal(err)
 	}
-	fmt.Printf("Get usernames: %+v\n", allnames)
+	if len(allnames) != 2 {
+		t.Fatal("expect names to be 2 but got ", len(allnames))
+	}
+	if err = writer.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestGetUUID(t *testing.T) {
-	dao, err := util.InitDB("172.16.101.98", "root", "password", "auth")
-	if err != nil {
-		t.Error("Cannot init auth db")
-		return
-	}
-
-	util.SetDB(ModuleInfo.ModuleName, dao)
+	var mockedRows = sqlmock.NewRows([]string{"uuid"}).AddRow("4b21158a395311e88a710242ac110003")
+	writer.ExpectQuery("SELECT uuid FROM users").WithArgs("csbotadmin").WillReturnRows(mockedRows)
 	uuid, err := GetUserID("csbotadmin")
 	if err != nil {
-		t.Error("GetUserID fail", err.Error())
+		t.Fatal("GetUserID fail", err.Error())
 	}
 	if uuid != "4b21158a395311e88a710242ac110003" {
 		t.Errorf("GetUserID except %+v, get %+v", "4b21158a395311e88a710242ac110003", uuid)
+	}
+	if err = writer.ExpectationsWereMet(); err != nil {
+		t.Fatal(err)
 	}
 }
