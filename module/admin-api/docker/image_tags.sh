11143967e3e88c6ba55cae0a60e4e4ab6e5ae4b8
Frank Chang <frankchang@emotibot.com>
2018-09-25 10:36:22 +0800

    admin-api: Update docker-compose build scripts

--- module/admin-api/docker/image_tags.sh
+++ module/admin-api/docker/image_tags.sh
@@ -4,11 +4,11 @@ GIT_HEAD="$(git rev-parse --short=7 HEAD)"
 GIT_DATE=$(git log HEAD -n1 --pretty='format:%cd' --date=format:'%Y%m%d-%H%M')
 
 # Use docker-reg for now, it will change to harbor after harbor usage is confirmed
-# ADMIN_REPO="harbor.emotibot.com/bfop"
-ADMIN_REPO="docker-reg.emotibot.com.cn:55688"
-ADMIN_CONTAINER="admin-api"
-ADMIN_TAG="${GIT_HEAD}_${GIT_DATE}"
+# REPO="harbor.emotibot.com/bfop"
+REPO="docker-reg.emotibot.com.cn:55688"
+CONTAINER="admin-api"
+TAG="${GIT_HEAD}_${GIT_DATE}"
 
-ADMIN_BUILD_IMAGE_NAME="${ADMIN_REPO}/${ADMIN_CONTAINER}_build:${ADMIN_TAG}"
-ADMIN_IMAGE_NAME="${ADMIN_REPO}/${ADMIN_CONTAINER}:${ADMIN_TAG}"
-ADMIN_CONTAINER_NAME=${ADMIN_CONTAINER}
+BUILD_IMAGE_NAME="${REPO}/${CONTAINER}_build:${TAG}"
+IMAGE_NAME="${REPO}/${CONTAINER}:${TAG}"
+CONTAINER_NAME=${CONTAINER}
