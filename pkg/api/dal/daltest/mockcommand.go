7846f6aa000bbd9539d9e8c7b0f561fc77227d0b
Su Ting Wen <zmes50416@gmail.com>
2018-09-27 14:22:09 +0800

    Add support commands
    support new commands:
      - Question
      - Questions
      - SimilarQuestions
      - SetSimilarQuestion

--- pkg/api/dal/daltest/mockcommand.go
+++ pkg/api/dal/daltest/mockcommand.go
@@ -8,10 +8,14 @@ import (
 	dal "emotibot.com/emotigo/pkg/api/dal/v1"
 )
 
+// mockCommand should create the correct http.Response based on inject result and expect behavior.
+// dal client then can interprate it.
+// TODO: Add a verify method to verify correct input.
 type mockCommand interface {
 	NewResponse() *http.Response
 }
 
+//closingBuffer is a warp struct to allow bytes.Buffer satisfy io.ReadCloser interface.
 type closingBuffer struct {
 	*bytes.Buffer
 }
@@ -22,35 +26,63 @@ func (c *closingBuffer) Close() error {
 
 var errNoSuccess = "OK"
 
+type execCommand struct {
+	result *ExpectResult
+}
 type deleteSimilarQuestionsCmd struct {
+	*execCommand
 	appID string
 	lq    []string
 }
 
-func (d *deleteSimilarQuestionsCmd) NewResponse() *http.Response {
+func (d *execCommand) NewResponse() *http.Response {
 	resp := &dal.RawResponse{
 		ErrNo: errNoSuccess,
 	}
 	expectResp, _ := json.Marshal(resp)
-
 	r := &closingBuffer{bytes.NewBuffer(expectResp)}
+	if d.result.hasErr {
+		return &http.Response{
+			Status:     "400 Bad Request",
+			StatusCode: http.StatusBadRequest,
+			Body:       &closingBuffer{&bytes.Buffer{}},
+		}
+	}
 	return &http.Response{
 		Status:     "200 OK",
 		StatusCode: http.StatusOK,
-		Header:     http.Header{},
 		Body:       r,
 	}
 }
 
+//matchCommand handle request of boolean response.
 type matchCommand struct {
 	result *ExpectResult
+	input  []interface{}
 }
 
 func (c *matchCommand) NewResponse() *http.Response {
-	resp := &dal.RawResponse{
-		ErrNo:     errNoSuccess,
-		Operation: []string{"OK"},
+	var resp *dal.RawResponse
+	if c.result.hasErr {
+		return &http.Response{
+			Status:     "500 Internal Server Error",
+			StatusCode: http.StatusInternalServerError,
+			Body:       &closingBuffer{&bytes.Buffer{}},
+		}
 	}
+	if c.result.values != nil && len(c.result.values) > 0 && c.result.values[0] == true {
+		resp = &dal.RawResponse{
+			ErrNo:     errNoSuccess,
+			Operation: []string{"OK"},
+		}
+	} else {
+		resp = &dal.RawResponse{
+			ErrNo:     "OTHER",
+			Operation: []string{"FAILED"},
+		}
+
+	}
+
 	data, _ := json.Marshal(resp)
 	return &http.Response{
 		Status:     "200 OK",
@@ -59,14 +91,63 @@ func (c *matchCommand) NewResponse() *http.Response {
 	}
 }
 
-// ExpectResult represent the result from dal module, which can provide find grind controll over mock result.
-type ExpectResult struct {
-	results    []interface{}
-	operations []string
+type queryQuestionsCommand struct {
+	result *ExpectResult
+	input  []interface{}
 }
 
-func (r *ExpectResult) WillReturn(Results []interface{}, Operations []string) {
-	//TODO: implement me
-	r.results = Results
-	r.operations = Operations
+func (c *queryQuestionsCommand) NewResponse() *http.Response {
+	var response dal.RawResponse
+	if c.result.values != nil {
+		response.ErrNo = errNoSuccess
+		for _, value := range c.result.values {
+			var record = map[string]interface{}{
+				"content": value,
+			}
+			response.Results = append(response.Results, record)
+		}
+		data, _ := json.Marshal(response)
+		cb := &closingBuffer{bytes.NewBuffer(data)}
+		return &http.Response{
+			Status:     "200 OK",
+			StatusCode: http.StatusOK,
+			Body:       cb,
+		}
+	}
+
+	return &http.Response{
+		Status:     "400 Bad Request",
+		StatusCode: http.StatusBadRequest,
+		Body:       &closingBuffer{&bytes.Buffer{}},
+	}
+}
+
+type questionByLQCommand struct {
+	result *ExpectResult
+}
+
+func (c *questionByLQCommand) NewResponse() *http.Response {
+	var response dal.RawResponse
+	if c.result.values != nil {
+		response.ErrNo = errNoSuccess
+		for _, value := range c.result.values {
+			var record = map[string]interface{}{
+				"parentContent": value,
+			}
+			response.Results = append(response.Results, record)
+		}
+		data, _ := json.Marshal(response)
+		cb := &closingBuffer{bytes.NewBuffer(data)}
+		return &http.Response{
+			Status:     "200 OK",
+			StatusCode: http.StatusOK,
+			Body:       cb,
+		}
+	}
+
+	return &http.Response{
+		Status:     "400 Bad Request",
+		StatusCode: http.StatusBadRequest,
+		Body:       &closingBuffer{&bytes.Buffer{}},
+	}
 }
--- pkg/api/dal/daltest/mocker.go
+++ pkg/api/dal/daltest/mocker.go
@@ -1,6 +1,14 @@
 /*
 Package daltest give support for mocking dal package without using httptest package.
 It should eliminate the need for developer to understand the dal networking or api endpoint.
+List of supported commands:
+	- DeleteSimilarQuestions(appID string, lq ...string) error
+	- IsSimilarQuestion(appID, lq string) (bool, error)
+	- IsStandardQuestion(appID, content string) (bool, error)
+	- Question(appID, lq string) (string, error)
+	- Questions(appID string) ([]string, error)
+	- SetSimilarQuestion(appID, sq string, lq ...string) error
+	- SimilarQuestions(appID string, sq string) ([]string, error)
 */
 package daltest
 
@@ -60,29 +68,66 @@ func (m *mockClient) Do(*http.Request) (*http.Response, error) {
 	return command.NewResponse(), nil
 }
 
-// List of need support command
-// 	[*] DeleteSimilarQuestions(appID string, lq ...string) error
-// 	[*] IsSimilarQuestion(appID, lq string) (bool, error)
-// 	IsStandardQuestion(appID, content string) (bool, error)
-// 	Question(appID, lq string) (string, error)
-// 	Questions(appID string) ([]string, error)
-// 	SetSimilarQuestion(appID, sq string, lq ...string) error
-// 	SimilarQuestions(appID string, sq string) ([]string, error)
-
 // ExpectDeleteSimilarQuestions expect delete similar question(相似問、擴寫) from dal client.
 // Since we can not verify the result yet, the input is not used.
-func (m *Mocker) ExpectDeleteSimilarQuestions(appID string, lq ...string) {
+func (m *Mocker) ExpectDeleteSimilarQuestions(appID string, lq ...string) *ExpectResult {
+	var result = &ExpectResult{}
 	var test = &deleteSimilarQuestionsCmd{
-		appID: appID,
-		lq:    lq,
+		execCommand: &execCommand{result: result},
+		appID:       appID,
+		lq:          lq,
 	}
 	m.expectCommands = append(m.expectCommands, test)
+	return result
 }
 
 // ExpectIsSimilarQuestion expect matching similar question from dal client
-// It will return a result for injecting match result
+// It will return a result for injecting match result.
+// Result should only contains one bool value, anymore will be ignored.
+// If result is empty or not bool, result will be false.
 func (m *Mocker) ExpectIsSimilarQuestion(appID, lq string) (result *ExpectResult) {
 	result = &ExpectResult{}
 	m.expectCommands = append(m.expectCommands, &matchCommand{result: result})
+	return result
+}
+
+// ExpectIsStandardQuestion expect matching content with standard questions from dal client.
+// It will return result for injecting match result.
+// Result should only contains one bool value, anymore will be ignored.
+// If result is empty or not bool, result will be false.
+func (m *Mocker) ExpectIsStandardQuestion(appID, content string) (result *ExpectResult) {
+	result = &ExpectResult{}
+	m.expectCommands = append(m.expectCommands, &matchCommand{result: result})
+	return result
+}
+
+// ExpectQuestion expect query question based on lq(similar question).
+// Result should be the content of expect question.
+func (m *Mocker) ExpectQuestion(appID, lq string) (result *ExpectResult) {
+	result = &ExpectResult{}
+	m.expectCommands = append(m.expectCommands, &questionByLQCommand{result: result})
+	return result
+}
+
+// ExpectQuestions expect query of all questions based on app ID.
+// Result should be all the content of expect questions.
+func (m *Mocker) ExpectQuestions(appID string) (result *ExpectResult) {
+	result = &ExpectResult{}
+	m.expectCommands = append(m.expectCommands, &queryQuestionsCommand{result: result})
+	return result
+}
+
+// ExpectSimilarQuestions expect query of lq based on sq(standard question)
+func (m *Mocker) ExpectSimilarQuestions(appID string, sq string) (result *ExpectResult) {
+	result = &ExpectResult{}
+	m.expectCommands = append(m.expectCommands, &queryQuestionsCommand{result: result})
+	return result
+}
+
+// ExpectSetSimilarQuestion expect create lq based on sq & app ID.
+// Result should be a single true of false.
+func (m *Mocker) ExpectSetSimilarQuestion(appID, sq string, lq ...string) (result *ExpectResult) {
+	result = &ExpectResult{}
+	m.expectCommands = append(m.expectCommands, &execCommand{result: result})
 	return result
 }
