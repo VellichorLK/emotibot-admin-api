b208ecb8bc478324f7be0882272a661efb0f55d7
Frank Chang <frankchang@emotibot.com>
2018-10-16 11:09:46 +0800

    admin-api: Add 'Content-Type' header to ELKStats responses

--- module/admin-api/ELKStats/controllers/controller.go
+++ module/admin-api/ELKStats/controllers/controller.go
@@ -8,21 +8,25 @@ import (
 )
 
 func returnOK(w http.ResponseWriter, resp interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	writeResponseJSON(w, resp)
 }
 
 func returnBadRequest(w http.ResponseWriter, errResp data.ErrorResponseWithCode) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusBadRequest)
 	writeResponseJSON(w, errResp)
 }
 
 func returnUnprocessableEntity(w http.ResponseWriter, errResp data.ErrorResponseWithCode) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusUnprocessableEntity)
 	writeResponseJSON(w, errResp)
 }
 
 func returnInternalServerError(w http.ResponseWriter, errResp data.ErrorResponse) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusInternalServerError)
 	writeResponseJSON(w, errResp)
 }
