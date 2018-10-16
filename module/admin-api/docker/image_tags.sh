8725573e624c6c4e2decd1f602b8f06a64292da6
DeanSu <deansu@emotibot.com>
2018-10-16 17:30:47 +0800

    update admin-api to use harbor.emotibot.com

--- module/admin-api/docker/image_tags.sh
+++ module/admin-api/docker/image_tags.sh
@@ -4,11 +4,11 @@ GIT_HEAD="$(git rev-parse --short=7 HEAD)"
 GIT_DATE=$(git log HEAD -n1 --pretty='format:%cd' --date=format:'%Y%m%d-%H%M')
 
 # Use docker-reg for now, it will change to harbor after harbor usage is confirmed
-# REPO="harbor.emotibot.com/bfop"
-REPO="docker-reg.emotibot.com.cn:55688"
+REPO="harbor.emotibot.com"
+PROJECT="bfop"
 CONTAINER="admin-api"
 TAG="${GIT_HEAD}-${GIT_DATE}"
 
-BUILD_IMAGE_NAME="${REPO}/${CONTAINER}-build:${TAG}"
-IMAGE_NAME="${REPO}/${CONTAINER}:${TAG}"
+BUILD_IMAGE_NAME="${REPO}/${PROJECT}/${CONTAINER}-build:${TAG}"
+IMAGE_NAME="${REPO}/${PROJECT}/${CONTAINER}:${TAG}"
 CONTAINER_NAME=${CONTAINER}
