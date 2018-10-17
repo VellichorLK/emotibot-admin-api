c2d90ee830e5f7fdd3b465ee3125c9249dd3ce68
DanielWu <i.lu.f@hotmail.com.tw>
2018-10-17 14:22:16 +0800

    FIX: Remove image after push

--- module/admin-api/docker/push_images.sh
+++ module/admin-api/docker/push_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source `pwd`/image_tags.sh;
 docker push $IMAGE_NAME && docker rmi $IMAGE_NAME;
-docker push $BUILD_IMAGE_NAME;
+docker push $BUILD_IMAGE_NAME && docker rmi $BUILD_IMAGE_NAME;
