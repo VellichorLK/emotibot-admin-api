6491a2f1d24aa60984e285dbfa58e7142d96d53a
taylorchung <taylorchung@emotibot.com>
2018-12-13 16:08:54 +0800

    add unit test for delete cache, destroy and reuse

--- module/qic-api/util/timecache/timeCache_test.go
+++ module/qic-api/util/timecache/timeCache_test.go
@@ -34,3 +34,42 @@ func TestTimeCache(t *testing.T) {
 	}
 
 }
+
+func TestTimeCacheDelete(t *testing.T) {
+	config := &TCacheConfig{}
+	config.SetCollectionDuration(2 * time.Second)
+	config.SetCollectionMethod(OnUpdate)
+
+	TCache.Activate(config)
+
+	TCache.SetCache("hello", "world")
+	v, ok := TCache.GetCache("hello")
+
+	if !ok {
+		t.Fatalf("[Unit test] getting key, hello, failed\n")
+	}
+	if v.(string) != "world" {
+		t.Fatalf("[Unit test] getting key value %s, but expecting world\n", v.(string))
+	}
+
+	TCache.DeleteCache("hello")
+	_, ok = TCache.GetCache("hello")
+	if ok {
+		t.Fatal("[Unit test] expecting no data for key hello, but get data\n")
+	}
+	TCache.Destroy()
+
+	//reuse
+	TCache.Activate(config)
+
+	TCache.SetCache("hello", "world")
+	v, ok = TCache.GetCache("hello")
+
+	if !ok {
+		t.Fatalf("[Unit test] getting key, hello, failed\n")
+	}
+	if v.(string) != "world" {
+		t.Fatalf("[Unit test] getting key value %s, but expecting world\n", v.(string))
+	}
+
+}
