8add8ebe10fd6616dd9b5650e133cfca39a59a80
DanielWu <i.lu.f@hotmail.com.tw>
2018-12-26 01:19:34 +0800

    FEATURE: Add api-key check and jwt check in admin-api

--- module/admin-api/auth/dao.go
+++ module/admin-api/auth/dao.go
@@ -1,10 +1,17 @@
 package auth
 
 import (
+	"database/sql"
 	"fmt"
 	"strings"
+	"time"
 
 	"emotibot.com/emotigo/module/admin-api/util"
+)
+
+var (
+	apiKeyCache = map[string]int64{}
+	apiKeyApp   = map[string]string{}
 )
 
 func GetUserNames(ids []string) (ret map[string]string, err error) {
@@ -98,4 +105,48 @@ func GetUserID(username string) (id string, err error) {
 	queryStr := fmt.Sprintf("SELECT uuid FROM users WHERE user_name = ?")
 	err = db.QueryRow(queryStr, username).Scan(&id)
 	return
+}
+
+var currentTimeGetter = getCurrentTimestamp
+
+func GetAppViaApiKey(apiKey string) (string, error) {
+	var err error
+	defer func() {
+		util.ShowError(err)
+	}()
+
+	currentTime := currentTimeGetter()
+	if expire, ok := apiKeyCache[apiKey]; ok {
+		if currentTime <= expire {
+			if appid, ok := apiKeyApp[apiKey]; ok {
+				return appid, nil
+			}
+		} else {
+			return "", sql.ErrNoRows
+		}
+	}
+
+	db := util.GetDB(ModuleInfo.ModuleName)
+	if db == nil {
+		err = util.ErrDBNotInit
+		return "", err
+	}
+
+	queryStr := "SELECT appid, expire_time FROM api_key WHERE api_key = ? AND expire_time > ?"
+	row := db.QueryRow(queryStr, apiKey, currentTime)
+	appid := ""
+	expire := int64(0)
+	err = row.Scan(&appid, &expire)
+	if err != nil {
+		return "", err
+	}
+
+	apiKeyCache[apiKey] = expire
+	apiKeyApp[apiKey] = appid
+
+	return appid, nil
+}
+
+func getCurrentTimestamp() int64 {
+	return time.Now().Unix()
 }
