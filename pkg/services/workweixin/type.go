3dc0fcd5389040cbc5d7db529acf02a298b247b2
DanielWu <i.lu.f@hotmail.com.tw>
2019-01-02 01:46:12 +0800

    FEATURE: Finish workweixin basic function

--- pkg/services/workweixin/type.go
+++ pkg/services/workweixin/type.go
@@ -1,18 +1,132 @@
 package workweixin
 
-type WorkWeixinMessageType string
+import "errors"
+
+// MessageType is use to tell the type of input message type
+type MessageType string
 
 const (
-	WorkWeixinMessageTypeText = "text"
+	MessageTypeText     MessageType = "text"
+	MessageTypeImage    MessageType = "image"
+	MessageTypeVoice    MessageType = "voice"
+	MessageTypeVedio    MessageType = "vedio"
+	MessageTypeLocation MessageType = "location"
+	MessageTypeLink     MessageType = "link"
 )
 
-type WorkWeixinMessage struct {
-	To         string                `xml:"ToUserName"`
-	From       string                `xml:"FromUserName"`
-	CreateTime int                   `xml:"CreateTime"`
-	Type       WorkWeixinMessageType `xml:"MsgType"`
+const (
+	MsgSendURL       = "https://qyapi.weixin.qq.com/cgi-bin/message/send"
+	TokenValidateURL = "https://open.work.weixin.qq.com/devtool/getInfoByAccessToken"
+	TokenIssueURL    = "https://qyapi.weixin.qq.com/cgi-bin/gettoken"
+)
+
+var (
+	ErrInvalidSignature = errors.New("Invalid signature")
+)
+
+type rawMessage struct {
+	To         string      `xml:"ToUserName,omitempty"`
+	From       string      `xml:"FromUserName,omitempty"`
+	CreateTime int64       `xml:"CreateTime,omitempty"`
+	Type       MessageType `xml:"MsgType,omitempty"`
+	AgentID    int         `xml:"AgentID,omitempty"`
+	MsgID      string      `xml:"MsgId,omitempty"`
 }
 
-type WorkWeixinTextMessage struct {
-	WorkWeixinMessage
+type Message interface {
+}
+
+type Input struct {
+	To        string `xml:"ToUserName"`
+	AgentID   string `xml:"AgentID"`
+	Encrypted string `xml:"Encrypt"`
+}
+
+type Output struct {
+	Encrypted string `xml:"Encrypt"`
+	Signature string `xml:"MsgSignature"`
+	Timestamp int64  `xml:"TimeStamp"`
+	Nonce     string `xml:"nonce"`
+}
+
+type TextMessage struct {
+	Message
+	rawMessage
+	Content string `xml:"Content"`
+}
+
+type ImageMessage struct {
+	Message
+	rawMessage
+	PicURL  string `xml:"PicUrl"`
+	MediaID string `xml:"MediaId"`
+}
+
+type VoiceMessage struct {
+	Message
+	rawMessage
+	Format  string `xml:"Format"`
+	MediaID string `xml:"MediaId"`
+}
+
+type VedioMessage struct {
+	Message
+	rawMessage
+	MediaID      string `xml:"MediaId"`
+	ThumbMediaID string `xml:"ThumbMediaId"`
+}
+
+type LocationMessage struct {
+	Message
+	rawMessage
+	Latitude  float32 `xml:"Location_X:"`
+	Longitude float32 `xml:"Location_Y"`
+	Scale     int     `xml:"Scale"`
+	Label     string  `xml:"Label"`
+}
+
+type LinkMessage struct {
+	Message
+	rawMessage
+	Title       string `xml:"Title"`
+	Description string `xml:"Description"`
+	PicURL      string `xml:"PicUrl"`
+}
+
+type SendingMessage interface {
+}
+
+type generalSendMessage struct {
+	To      string      `json:"touser"`
+	Type    MessageType `json:"msgtype"`
+	AgentID int         `json:"agentid"`
+	safe    int         `json:"safe"`
+}
+
+type TextNode struct {
+	Content string `json:"content"`
+}
+
+type TextSendMessage struct {
+	SendingMessage
+	generalSendMessage
+	Text *TextNode `json:"text"`
+}
+
+type APIReturn struct {
+	ErrCode int    `json:"errcode"`
+	ErrMsg  string `json:"errmsg"`
+}
+
+type APIChatReturn struct {
+	APIReturn
+	InvalidUser  string `json:"invaliduser"`
+	InvalidParty string `json:"invalidparty"`
+	InvalidTag   string `json:"invalidtag"`
+}
+
+type APIAccessTokenReturn struct {
+	APIReturn
+	AccessToken string `json:"access_token"`
+	Expire      int64  `json:"expires_in"`
 }
--- pkg/services/workweixin/workweixin.go
+++ pkg/services/workweixin/workweixin.go
@@ -1,17 +1,21 @@
 package workweixin
 
 import (
+	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
 	"crypto/sha1"
 	"encoding/base64"
 	"encoding/binary"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
 	"sort"
 	"strings"
+	"time"
 
 	"emotibot.com/emotigo/pkg/logger"
 )
@@ -21,12 +25,19 @@ type Client struct {
 	EncodingAES string
 	AESKey      []byte
 	Cipher      cipher.Block
+
+	CorpID      string
+	Secret      string
+	AccessToken string
+	ExpireTime  int64
 }
 
-func New(token, encodingAES string) (*Client, error) {
+func New(corpid, secret, token, encodingAES string) (*Client, error) {
 	client := &Client{}
 	client.Token = token
 	client.EncodingAES = encodingAES
+	client.Secret = secret
+	client.CorpID = corpid
 
 	var err error
 	client.AESKey, err = base64.StdEncoding.DecodeString(encodingAES)
@@ -111,12 +122,170 @@ func calculateSignature(token, timestamp, nonce, message string) string {
 	return signature
 }
 
-func (c *Client) ParseRequest(r *http.Request) ([]*WorkWeixinMessage, error) {
-	_, err := ioutil.ReadAll(r.Body)
+func (c *Client) getPostMsg(r *http.Request) ([]byte, error) {
+	content, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 
+	input := Input{}
+	err = xml.Unmarshal(content, &input)
 	if err != nil {
-		return nil, nil
+		return nil, err
+	}
+	signature := r.URL.Query().Get("msg_signature")
+	timestamp := r.URL.Query().Get("timestamp")
+	nonce := r.URL.Query().Get("nonce")
+	encryptStr := input.Encrypted
+
+	logger.Trace.Printf(`Verify with:
+	signature:	%s
+	timestamp: %s
+	nonce: %s
+	encryptStr: %s
+	`, signature, timestamp, nonce, encryptStr)
+	verify := calculateSignature(c.Token, timestamp, nonce, encryptStr)
+	logger.Trace.Printf("Signature check: %s, %s\n", verify, signature)
+	if verify != signature {
+		return nil, ErrInvalidSignature
+	}
+
+	msg, _, err := decrypt(c.Cipher, c.AESKey, encryptStr)
+	if err != nil {
+		logger.Error.Printf("Decrypt error: %s\n", err.Error())
+		return nil, err
+	}
+	return msg, nil
+}
+
+func (c *Client) ParseRequest(r *http.Request) (Message, error) {
+	input, err := c.getPostMsg(r)
+	if err != nil {
+		return nil, err
+	}
+
+	rawMsg := rawMessage{}
+	err = xml.Unmarshal(input, &rawMsg)
+	if err != nil {
+		return nil, err
+	}
+
+	switch rawMsg.Type {
+	case MessageTypeText:
+		textMsg := TextMessage{}
+		err = xml.Unmarshal(input, &textMsg)
+		if err != nil {
+			return nil, err
+		}
+		logger.Trace.Printf("Receive %+v\n", textMsg)
+		return &textMsg, nil
+	case MessageTypeImage:
+
+	}
+
+	return nil, nil
+}
+
+func NewTextMessage(receiver string, agentID int, text string) SendingMessage {
+	ret := TextSendMessage{}
+	ret.To = receiver
+	ret.Type = MessageTypeText
+	ret.AgentID = agentID
+	ret.Text = &TextNode{text}
+	return &ret
+}
+
+func (c *Client) SendMessages(messages []SendingMessage) (*APIChatReturn, error) {
+	for idx := range messages {
+		if messages[idx] == nil {
+			continue
+		}
+		input, err := json.Marshal(messages[idx])
+		if err != nil {
+			logger.Error.Println("Marshal json fail:", err.Error())
+			return nil, err
+		}
+		return c.Post(MsgSendURL, input)
 	}
 	return nil, nil
+}
+
+func (c *Client) Post(url string, input []byte) (*APIChatReturn, error) {
+	var err error
+	if !c.AccessTokenValidate() {
+		err = c.GetNewAccessToken()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	reader := bytes.NewReader(input)
+	realURL := fmt.Sprintf("%s?access_token=%s", url, c.AccessToken)
+	logger.Trace.Printf("Send request to: %s, with body: %s\n", realURL, input)
+
+	rsp, err := http.Post(realURL, "application/json", reader)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
+	body, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return nil, err
+	}
+	logger.Trace.Printf("Post get: %s\n", body)
+	ret := APIChatReturn{}
+	err = json.Unmarshal(body, &ret)
+	if err != nil {
+		return nil, err
+	}
+	return &ret, nil
+}
+
+// AccessTokenValidate is a hacked method to check token validation
+func (c *Client) AccessTokenValidate() bool {
+	// if c.AccessToken == "" {
+	// 	return false
+	// }
+	// rsp, err := http.Get(fmt.Sprintf("%s?access_token=%s", TokenValidateURL, c.AccessToken))
+	// if err != nil {
+	// 	logger.Error.Println("Check access token validation fail,", err.Error())
+	// 	return false
+	// }
+	// defer rsp.Body.Close()
+	// content, err := ioutil.ReadAll(rsp.Body)
+	// if err != nil {
+	// 	logger.Error.Println("Read body error,", err.Error())
+	// 	return false
+	// }
+	// if bytes.Contains(content, []byte("data")) {
+	// 	return true
+	// }
+	// return false
+	now := time.Now()
+	return now.Unix() < c.ExpireTime
+}
+
+func (c *Client) GetNewAccessToken() error {
+	now := time.Now()
+	url := fmt.Sprintf("%s?corpid=%s&corpsecret=%s", TokenIssueURL, c.CorpID, c.Secret)
+	logger.Trace.Println("Get token with url:", url)
+	rsp, err := http.Get(url)
+	if err != nil {
+		logger.Error.Println("Get new access token request fail,", err.Error())
+		return err
+	}
+	defer rsp.Body.Close()
+
+	decoder := json.NewDecoder(rsp.Body)
+	ret := APIAccessTokenReturn{}
+	err = decoder.Decode(&ret)
+	if err != nil {
+		logger.Error.Println("Decode return fail:", err.Error())
+		return err
+	}
+
+	c.AccessToken = ret.AccessToken
+	// minus 10 second to avoid API latency
+	c.ExpireTime = ret.Expire + now.Unix() - 10
+
+	logger.Trace.Printf("Get new access token: %s, %d\n", c.AccessToken, c.ExpireTime)
+	return nil
 }
