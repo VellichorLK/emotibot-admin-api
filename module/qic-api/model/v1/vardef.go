8350a22e2bf50da748aef03dc7e80a0f39b64352
kenchang <kenchang@emotibot.com>
2019-01-07 16:42:11 +0800

    create inspect task api

--- module/qic-api/model/v1/vardef.go
+++ module/qic-api/model/v1/vardef.go
@@ -28,6 +28,8 @@ const (
 	tblRelCFSG          = "Relation_ConversationFlow_SentenceGroup"
 	tblConversationRule = "Rule"
 	tblRelCRCF          = "Relation_Rule_ConversationFlow"
+	tblInspectTask      = "InspectorTask"
+	tblRelITOuline      = "Relation_InspectorTask_Outline"
 )
 
 //field name in Conversation table
@@ -191,13 +193,15 @@ const (
 
 //common field name
 const (
-	fldID         = "id"
-	fldIsDelete   = "is_delete"
-	fldName       = "name"
-	fldEnterprise = "enterprise"
-	fldUUID       = "uuid"
-	fldCreateTime = "create_time"
-	fldUpdateTime = "update_time"
+	fldID          = "id"
+	fldIsDelete    = "is_delete"
+	fldName        = "name"
+	fldEnterprise  = "enterprise"
+	fldUUID        = "uuid"
+	fldCreateTime  = "create_time"
+	fldUpdateTime  = "update_time"
+	fldDescription = "description"
+	fldCreator     = "creator"
 )
 
 //relation field name
@@ -232,4 +236,23 @@ const (
 const (
 	CRCFRID  = "rule_id"
 	CRCFCFID = "cf_id"
+)
+
+// fields in InspectTask
+const (
+	ITCallStart         = "call_start"
+	ITCallEnd           = "call_end"
+	ITInspectPercentage = "inspector_percentage"
+	ITInspectByPerson   = "inspector_byperson"
+	ITPublishTime       = "publish_time"
+	ITReviewPercentage  = "review_percentage"
+	ITReviewByPerson    = "review_byperson"
+	ITExcluedInspected  = "exclude_inspected"
+	ITFormID            = "form_id"
+)
+
+// fields in Relation_InspectorTask_Outline
+const (
+	RITOTaskID   = "task_id"
+	RITOOulineID = "outline_id"
 )
