f33107c3d7c0cc9d3d4e9e277d66049e8f566b74
kenchang <kenchang@emotibot.com>
2019-01-15 16:49:37 +0800

    1. add category api 2. get categories api 3. get category sentences api

---
+++ module/qic-api/qi/category_controller.go
@@ -0,0 +1,127 @@
+package qi
+
+import (
+	"emotibot.com/emotigo/module/admin-api/util"
+	"emotibot.com/emotigo/module/admin-api/util/requestheader"
+	"emotibot.com/emotigo/module/qic-api/model/v1"
+	"emotibot.com/emotigo/module/qic-api/util/general"
+	"emotibot.com/emotigo/module/qic-api/util/request"
+	"emotibot.com/emotigo/pkg/logger"
+	"net/http"
+	"strconv"
+)
+
+func handleCreateCategory(w http.ResponseWriter, r *http.Request) {
+	enterprise := requestheader.GetEnterpriseID(r)
+	if enterprise == "" {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	category := model.CategortInfo{}
+	err := util.ReadJSON(r, &category)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	category.Enterprise = enterprise
+	categoryID, err := CreateCategory(&category)
+	if err != nil {
+		logger.Error.Printf("error while create category in handleCreateCategory, reason: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type Response struct {
+		ID int64 `json:"category_id"`
+	}
+
+	response := Response{
+		ID: categoryID,
+	}
+
+	util.WriteJSON(w, response)
+}
+
+func handleGetCategoryies(w http.ResponseWriter, r *http.Request) {
+	enterprise := requestheader.GetEnterpriseID(r)
+	if enterprise == "" {
+		http.Error(w, "no enterprise", http.StatusBadRequest)
+		return
+	}
+
+	isDelete := int8(0)
+	query := &model.CategoryQuery{
+		Enterprise: &enterprise,
+		IsDelete:   &isDelete,
+	}
+
+	categories, err := GetCategories(query)
+	if err != nil {
+		logger.Error.Printf("error while get categories in handleGetCategories, reason: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	util.WriteJSON(w, categories)
+}
+
+func handleGetCategory(w http.ResponseWriter, r *http.Request) {
+	enterprise := requestheader.GetEnterpriseID(r)
+	idStr := general.ParseID(r)
+	id, err := strconv.ParseUint(idStr, 10, 64)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	paging := request.Paging(r)
+
+	isDelete := int8(0)
+	query := &model.SentenceQuery{
+		CategoryID: &id,
+		Enterprise: &enterprise,
+		IsDelete:   &isDelete,
+		Page:       paging.Page,
+		Limit:      paging.Limit,
+	}
+
+	total, sentences, err := GetCategorySentences(query)
+	if err != nil {
+		logger.Error.Printf("error while get category sentences in handleGetCategory, reason: %s", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type Response struct {
+		Paging *general.Paging        `json:"paging"`
+		Data   []model.SimpleSentence `json:"data"`
+	}
+
+	simpleSentences := []model.SimpleSentence{}
+	for _, sentence := range sentences {
+		simpleSentence := model.SimpleSentence{
+			UUID:       sentence.UUID,
+			Name:       sentence.Name,
+			CategoryID: sentence.CategoryID,
+		}
+		simpleSentences = append(simpleSentences, simpleSentence)
+	}
+
+	paging.Total = int64(total)
+	response := Response{
+		Paging: paging,
+		Data:   simpleSentences,
+	}
+
+	util.WriteJSON(w, response)
+}
+
+func handleUpdateCatgory(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func handleDeleteCategory(w http.ResponseWriter, r *http.Request) {
+
+}
---
+++ module/qic-api/qi/category_service.go
@@ -0,0 +1,36 @@
+package qi
+
+import (
+	"emotibot.com/emotigo/module/qic-api/model/v1"
+)
+
+var categoryDao model.CategoryDao = &model.CategorySQLDao{}
+
+func GetCategories(query *model.CategoryQuery) (categories []*model.CategortInfo, err error) {
+	sqlConn := dbLike.Conn()
+	return categoryDao.GetCategories(sqlConn, query)
+}
+
+func CreateCategory(category *model.CategortInfo) (id int64, err error) {
+	sqlConn := dbLike.Conn()
+	c := &model.CategoryRequest{
+		Name:       category.Name,
+		Enterprise: category.Enterprise,
+	}
+	return categoryDao.InsertCategory(sqlConn, c)
+}
+
+func GetCategorySentences(query *model.SentenceQuery) (total uint64, sentences []*model.Sentence, err error) {
+	tx, err := dbLike.Begin()
+	if err != nil {
+		return
+	}
+
+	total, err = sentenceDao.CountSentences(tx, query)
+	if err != nil {
+		return
+	}
+
+	sentences, err = sentenceDao.GetSentences(tx, query)
+	return
+}
