f21d3ef012f1762a65a7fa1cae93982dbcc2c891
Frank Chang <frankchang@emotibot.com>
2019-02-19 18:44:27 +0800

    admin-api: [zhconverter] Move zhConverter under zhconverter package

--- module/admin-api/autofill/autofill.go
+++ module/admin-api/autofill/autofill.go
@@ -14,6 +14,7 @@ import (
 	"emotibot.com/emotigo/module/admin-api/autofill/data"
 	"emotibot.com/emotigo/module/admin-api/util"
 	"emotibot.com/emotigo/module/admin-api/util/solr"
+	"emotibot.com/emotigo/module/admin-api/util/zhconverter"
 	"emotibot.com/emotigo/pkg/logger"
 )
 
@@ -327,11 +328,11 @@ func resetIntentAutofills(appID string) error {
 				}
 				body.RelatedSentences = string(relatedSentenceJSON)
 
-				body.Sentence = strings.ToLower(util.T2S(nluResult.Keyword.ToString()))
+				body.Sentence = strings.ToLower(zhconverter.T2S(nluResult.Keyword.ToString()))
 				body.SentenceCU = "{}"
-				body.SentenceKeywords = strings.ToLower(util.T2S(nluResult.Keyword.ToString()))
-				body.SentenceType = strings.ToLower(util.T2S(nluResult.SentenceType))
-				body.SentencePos = strings.ToLower(util.T2S(nluResult.Segment.ToFullString()))
+				body.SentenceKeywords = strings.ToLower(zhconverter.T2S(nluResult.Keyword.ToString()))
+				body.SentenceType = strings.ToLower(zhconverter.T2S(nluResult.SentenceType))
+				body.SentencePos = strings.ToLower(zhconverter.T2S(nluResult.Segment.ToFullString()))
 				body.Source = time.Now().Format("2006-01-02 15:04:05")
 
 				// Autofill enabled
