6e2ca9c33fa6afba93f414b7c725a0e99b8701c2
Daniel Wu <i.lu.f@hotmail.com.tw>
2019-03-18 14:08:02 +0800

    FIX: Remove error comment

--- module/token-auth/docker/image_tags.sh
+++ module/token-auth/docker/image_tags.sh
@@ -3,7 +3,6 @@
 GIT_HEAD="$(git rev-parse --short=7 HEAD)"
 GIT_DATE=$(git log HEAD -n1 --pretty='format:%cd' --date=format:'%Y%m%d-%H%M')
 
-# Use docker-reg for now, it will change to harbor after harbor usage is confirmed
 REPO="harbor.emotibot.com"
 PROJECT="bfop"
 CONTAINER="token-auth"
