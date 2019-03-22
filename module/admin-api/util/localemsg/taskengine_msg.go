0bd66602231ccc59d8091c336fcc535c76d83f7c
Daniel Wu <i.lu.f@hotmail.com.tw>
2019-03-22 14:34:25 +0800

    FIX: Fix wordbank zh-cn/zh-tw problem when task-engine getting mapping table

---
+++ module/admin-api/util/localemsg/taskengine_msg.go
@@ -0,0 +1,12 @@
+package localemsg
+
+var taskengineMsg = map[string]map[string]string{
+	"zh-cn": map[string]string{
+		"TaskEngineWordbank": "任务引擎词库",
+		"TaskEngineScenario": "任务引擎场景",
+	},
+	"zh-tw": map[string]string{
+		"TaskEngineWordbank": "任務引擎詞庫",
+		"TaskEngineScenario": "任務引擎場景",
+	},
+}
