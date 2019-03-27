4fe65a5ba71793feb69a3e36bfeca401231d9a1d
zhihongyuan <zhihongyuan@emotibot.com>
2019-03-27 17:39:35 +0800

    password Compare change to EqualFold

--- module/vipshop-fake-cas/server.go
+++ module/vipshop-fake-cas/server.go
@@ -42,7 +42,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if strings.Compare(parameters["appid"][0], "vca") != 0 {
+	if strings.EqualFold(parameters["appid"][0], "vca") {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintln(w, "appid incorrect")
 		return
