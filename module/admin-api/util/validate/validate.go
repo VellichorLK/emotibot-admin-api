c56e72c9100e2c8727bc4d0fcdc040ff022c30f4
wenkong <wenkong@emotibot.com>
2019-06-10 14:29:24 +0800

    *)在路由中判断appid是否存在，不存在appid无法通过鉴权

--- module/admin-api/util/validate/validate.go
+++ module/admin-api/util/validate/validate.go
@@ -1,5 +1,22 @@
 package validate
 
+import (
+	"emotibot.com/emotigo/module/admin-api/util"
+)
+
 func IsValidAppID(id string) bool {
+	db := util.GetAuditDB()
+	if db == nil {
+		return false
+	}
+
+	appsql := "SELECT uuid FROM auth.apps WHERE uuid = ?"
+	nums, err := db.Query(appsql, id)
+	if err != nil {
+		return false
+	}
+	if !nums.Next() {
+		return false
+	}
 	return len(id) > 0 && HasOnlyNumEngDash(id)
 }
