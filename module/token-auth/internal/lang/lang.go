b6fb08150f7dbc51aced512bdee53449f887a32e
xunmin wang <xunminwang@emotibot.com>
2019-07-09 21:33:08 +0800

    feature/auth_update
    改造动态左侧菜单栏

--- module/token-auth/internal/lang/lang.go
+++ module/token-auth/internal/lang/lang.go
@@ -1,9 +1,9 @@
 package lang
 
 import (
+	"emotibot.com/emotigo/module/token-auth/internal/util"
 	"encoding/json"
 	"io/ioutil"
-	"os"
 	"path/filepath"
 )
 
@@ -11,15 +11,18 @@ var LG map[string]map[string]string
 
 // 初始化语言包
 func LoadLang() map[string]map[string]string {
-	dir, err := filepath.Abs(filepath.Dir(os.Getenv("GOPATH") + "/src/emotibot.com/emotigo/module/token-auth/internal/lang"))
-	if err != nil {
-
-	}
-	dir = dir + "/lang"
+	//dir, err := filepath.Abs(filepath.Dir(os.Getenv("GOPATH") + "/src/emotibot.com/emotigo/module/token-auth/internal/lang"))
+	dir := "template/lang"
+	//if err != nil {
+	//	util.LogInfo.Println("lang wrong path")
+	//	return nil
+	//}
+	//dir = dir + "/lang"
 
 	dirs, err := ioutil.ReadDir(dir)
 	if err != nil {
-
+		util.LogInfo.Println("lang cannot read dir")
+		return nil
 	}
 
 	langDirs := map[string]string{}
@@ -37,13 +40,15 @@ func LoadLang() map[string]map[string]string {
 				filePath := v + "/" + vv.Name()
 				fileBytes, err := ioutil.ReadFile(filePath)
 				if err != nil {
-
+					util.LogInfo.Println("lang cannot read file")
+					return nil
 				}
 
 				var mapLang map[string]string
 				err = json.Unmarshal(fileBytes, &mapLang)
 				if err != nil {
-
+					util.LogInfo.Println("lang cannot json unmarshal fail")
+					return nil
 				}
 				if _, ok := LG[local]; ok {
 					for kkk, vvv := range mapLang {
@@ -65,9 +70,11 @@ func LoadLang() map[string]map[string]string {
 // 获取语言对应文字
 func Get(local string, key string) string {
 	if _, ok := LG[local]; !ok {
+		util.LogInfo.Println("lang get no local")
 		local = "zh-cn"
 	}
 	if _, ok := LG[local][key]; !ok {
+		util.LogInfo.Println("lang get no key")
 		return ""
 	}
 
